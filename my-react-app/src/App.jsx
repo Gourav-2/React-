2593bcdaf0e97bc7fdaa7bdf4267e034e23a965a
Gourav-2 <gc27112006@gmail.com>
2026-06-10 10:23:14 +0530

    code

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -386,30 +386,61 @@ import { useState } from "react"
 // export default App
 
 
+// import React from 'react'
+// import Home from "./Home"
+
+// //main-> app-> A-> b-> C
+
+
+
+// import NavBar from "./NavBar"
+
+// import { Route, Routes } from "react-router-dom"
+// import Cart from "./Cart"
+
+// const App = () => {
+//   let [apiData,SetApiData] =   useState([])
+//   let [cart,SetCart]= useState([])
+//   return (
+
+//     <div>
+//       <NavBar/>
+//       <Routes>
+//         <Route path="/" element={<Home apiData={apiData}  SetApiData={SetApiData} cart={cart}  SetCart={SetCart}/>}/>
+//         <Route path="/cart" element={<Cart cart={cart}/>}/>
+
+//       </Routes>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from 'react'
+// import Todo from "./Todo_3"
+// import Todo_3 from "./Todo_3"
+
+// const App = () => {
+//   return (
+//     <div>
+
+//   <Todo_3/>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 import React from 'react'
-import Home from "./Home"
-
-//main-> app-> A-> b-> C
-
-
-
-import NavBar from "./NavBar"
-
-import { Route, Routes } from "react-router-dom"
-import Cart from "./Cart"
+import Todo from './Todo'
 
 const App = () => {
-  let [apiData,SetApiData] =   useState([])
-  let [cart,SetCart]= useState([])
   return (
-
     <div>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home apiData={apiData}  SetApiData={SetApiData} cart={cart}  SetCart={SetCart}/>}/>
-        <Route path="/cart" element={<Cart cart={cart}/>}/>
-
-      </Routes>
+      <Todo/>
     </div>
   )
 }
--- my-react-app/src/Home.jsx
+++ my-react-app/src/Home.jsx
@@ -1,56 +1,56 @@
-// import React from 'react'
+// // import React from 'react'
 
-// const Home = () => {
-//   return (
-//     <div>Home</div>
-//   )
-// }
-// const New = () => {
-//     return (
-//       <div>newww fileeeee</div>
-//     )
-//   }
-//   export {New}
+// // const Home = () => {
+// //   return (
+// //     <div>Home</div>
+// //   )
+// // }
+// // const New = () => {
+// //     return (
+// //       <div>newww fileeeee</div>
+// //     )
+// //   }
+// //   export {New}
 
-// export default Home
+// // export default Home
 
-import React, { useEffect, useState } from 'react'
-import './App.css'
-const Home = ({apiData,SetApiData,cart,SetCart}) => {
+// import React, { useEffect, useState } from 'react'
+// import './App.css'
+// const Home = ({apiData,SetApiData,cart,SetCart}) => {
 
-  console.log(cart,"carttttt");
+//   console.log(cart,"carttttt");
   
      
 
     
-  useEffect(()=>{
-   async function apiCall(){
+//   useEffect(()=>{
+//    async function apiCall(){
 
-  let res=   await   fetch("https://dummyjson.com/products")
-       let data=       await res.json()
-       console.log(data);
-       SetApiData(data.products)
+//   let res=   await   fetch("https://dummyjson.com/products")
+//        let data=       await res.json()
+//        console.log(data);
+//        SetApiData(data.products)
        
 
-    }
-    apiCall()
+//     }
+//     apiCall()
 
-  },[])
+//   },[])
 
-  return (
-    <div id='card'>
-      {
-        apiData.map((a)=>{
-          return(<div id='main_card'>
-          <img   src={a.thumbnail}/>
-          {/* <h4>{a.title}</h4>
-          <h6>{a.price}</h6> */}
-          <button onClick={()=>SetCart([...cart,a])}>add</button>
-          </div>)
-        })
-      }
-    </div>
-  )
-}
+//   return (
+//     <div id='card'>
+//       {
+//         apiData.map((a)=>{
+//           return(<div id='main_card'>
+//           <img   src={a.thumbnail}/>
+//           {/* <h4>{a.title}</h4>
+//           <h6>{a.price}</h6> */}
+//           <button onClick={()=>SetCart([...cart,a])}>add</button>
+//           </div>)
+//         })
+//       }
+//     </div>
+//   )
+// }
 
-export default Home
+// export default Home
